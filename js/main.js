958273b579769d8e1d4f111443ef7a9d21ab62a4
Jorge Lopez-Vargas <jalopez2@utpl.edu.ec>
2018-12-20 23:02:29 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+Vue.component('q-option', {
+    props: ['option','groupid'],
+    template: `
+        <div>
+            <input type="radio" :name="groupid" :value="option.id"> {{ option.text }}
+        </div>        
+                       
+    `
+});
+
+new Vue({
+    el: "#app",
+    data: {
+        status: "",
+        key: "",
+        survey:{}
+    },
+    methods: {
+        connect() {
+            socket = new WebSocket("ws://localhost:4567/survey");
+            socket.onopen = this.openWs;
+            socket.onerror = this.errorWs;
+            socket.onmessage = this.messageWs;
+        },
+        openWs() {
+            this.status = 'connected';
+            console.log('connected');
+            this.sendMessage(this.status);
+        },
+        errorWs(evt) {
+            this.status = 'error';
+            console.log(evt.data);
+        },
+        messageWs(evt) {
+            this.survey = JSON.parse(evt.data);
+            console.log(evt.data);
+        },
+        sendMessage(msgData) {
+            socket.send(msgData);
+        }
+    }
+});
+
