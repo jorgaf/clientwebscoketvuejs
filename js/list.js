76f37a145513e56bc465a00334dcbe704978d0de
Jorge Lopez-Vargas <jalopez2@utpl.edu.ec>
2019-01-13 17:31:08 -0500

    Add GUI to delete

--- js/list.js
+++ js/list.js
@@ -1,8 +1,19 @@
 Vue.http.headers.common['Accept'] = 'application/json';
 
+
+
 Vue.component('student', {
     template: "#template-student",
     props: ['student'],
+    methods: {
+        deleteStudent: function (student) {
+            var index = vm.students.indexOf(student);
+            vm.students.splice(index, 1);
+            Vue.http.headers.common['Accept'] = 'application/json';
+            this.$http.delete('http://localhost:4567/student/'+index,
+                {headers: {'Accept': 'application/json'}} );
+        }
+    }
 });
 
 
@@ -19,7 +30,11 @@ var vm = new Vue({
             this.$http.get('http://localhost:4567/students').
                 then(function (response) {
                     console.log(response);
-                    Vue.set(vm, 'students', response.data);
+                    if(response.status === 200) {
+                        Vue.set(vm, 'students', response.data);
+                    } else {
+                        alert('Error');
+                    }
             })
         }
     }
